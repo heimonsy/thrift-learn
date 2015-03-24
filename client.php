8d13099d922041450f5475102e1292d2039b2559
heimonsy <heimonsy@gmail.com>
2015-03-24 11:15:24 +0800

    first commit

---
+++ client.php
@@ -0,0 +1,56 @@
+#!/usr/bin/env php
+<?php
+error_reporting(E_ALL);
+
+include 'vendor/autoload.php';
+
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Transport\TSocket;
+use Thrift\Transport\THttpClient;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Exception\TException;
+
+try {
+    if (array_search('--http', $argv)) {
+        $socket = new THttpClient('localhost', 8080, '/php/PhpServer.php');
+    } else {
+        $socket = new TSocket('localhost', 9090);
+    }
+    $transport = new TBufferedTransport($socket, 1024, 1024);
+    $protocol = new TBinaryProtocol($transport);
+    $client = new \tutorial\CalculatorClient($protocol);
+
+    $transport->open();
+
+    $client->ping();
+    print "ping()\n";
+
+    $sum = $client->add(1,1);
+    print "1+1=$sum\n";
+
+    $work = new \tutorial\Work();
+
+    $work->op = \tutorial\Operation::DIVIDE;
+    $work->num1 = 1;
+    $work->num2 = 0;
+
+    try {
+        $client->calculate(1, $work);
+        print "Whoa! We can divide by zero?\n";
+    } catch (\tutorial\InvalidOperation $io) {
+        print "InvalidOperation: $io->why\n";
+    }
+
+    $work->op = \tutorial\Operation::SUBTRACT;
+    $work->num1 = 15;
+    $work->num2 = 10;
+    $diff = $client->calculate(1, $work);
+    print "15-10=$diff\n";
+
+    $log = $client->getStruct(1);
+    print "Log: $log->value\n";
+
+    $transport->close();
+} catch (TException $tx) {
+    print 'TException: '.$tx->getMessage()."\n";
+}
